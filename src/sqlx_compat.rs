1bb039779f654c2378ac04d7b83060a14bfd26d5
Saagar <269905221+saagpatel@users.noreply.github.com>
2026-09-06 00:02:32 -0700

    fix: align SQLx compatibility with 0.9 (#53)

--- src/sqlx_compat.rs
+++ src/sqlx_compat.rs
@@ -1,6 +1,8 @@
 pub use sqlx_core::error::Error;
 pub use sqlx_core::from_row::FromRow;
 pub use sqlx_core::row::Row;
+pub use sqlx_core::sql_str::AssertSqlSafe;
+use sqlx_core::sql_str::SqlSafeStr;
 pub use sqlx_postgres::PgPool;
 use sqlx_postgres::{PgRow, Postgres};
 
@@ -12,20 +14,19 @@ pub mod postgres {
     pub use sqlx_postgres::{PgPoolOptions, PgRow};
 }
 
-pub fn query(
-    sql: &str,
-) -> sqlx_core::query::Query<'_, Postgres, <Postgres as sqlx_core::database::Database>::Arguments<'_>>
-{
+pub fn query<'q>(
+    sql: impl SqlSafeStr,
+) -> sqlx_core::query::Query<'q, Postgres, <Postgres as sqlx_core::database::Database>::Arguments> {
     sqlx_core::query::query::<Postgres>(sql)
 }
 
 pub fn query_as<'q, O>(
-    sql: &'q str,
+    sql: impl SqlSafeStr,
 ) -> sqlx_core::query_as::QueryAs<
     'q,
     Postgres,
     O,
-    <Postgres as sqlx_core::database::Database>::Arguments<'q>,
+    <Postgres as sqlx_core::database::Database>::Arguments,
 >
 where
     O: for<'r> FromRow<'r, PgRow>,
